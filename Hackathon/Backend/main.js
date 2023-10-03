472270c2d79400c1576ee4f6689d6d34b02226db
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-10-03 13:14:00 +0200

    Update tree

--- Hackathon/Backend/main.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
-
-const app = require('./public.src/js/App.js');
-
-app.listen(process.env.APP_PORT || 3000,  () => {
-    console.log(`App running on port ${process.env.PORT}...`);
-})
--- Hackathon/Frontend/src/pages/register.js
+++
@@ -1,3 +0,0 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Button, ImageBackground } from 'react-native';
-

0dc388ce6966b044a5af00fe398d26440e439dd3
NRIVALANT <nicolas.rivalant@ynov.com>
2023-10-05 10:43:35 +0200

    AddEvents

--- Frontend/src/03.globals/01.Pages/AddEvent.jsx
+++ Frontend/src/03.globals/01.Pages/AddEvent.jsx
@@ -16,43 +16,85 @@ const AddEvent = () => {
             .catch((err) => console.log(err));
     };
     
-
+    
+    const styles = {
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            maxWidth: '800px', 
+            margin: '0 auto', 
+            padding: '20px', 
+          },
+          row: {
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: '10px', 
+          },
+          date: {
+            flex: '1',
+            textAlign: 'center',
+          },
+          image: {
+            flex: '1',
+            marginLeft: '20px', 
+          },
+          title: {
+            flex: '1',
+            marginLeft: '20px', 
+          },
+          description: {
+            flex: '1',
+          }
+      };
+    
     return (
-        <div>
+        <div style={styles.container}>
             <h1>Envoyer un post</h1>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Titre"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    name="name"
-                />
-                <input
-                    type="datetime-local"
-                    placeholder="Date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                    name="date"
-                />
-                <input 
-                    type="text"
-                    placeholder="Description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    name="description"
-                />
-                
-                <input 
-                    type="file" 
-                    onChange={(e) => setImage(e.target.files[0])}
-                    name="image"
-                />
-                <button type="submit">Envoyer</button>
+                <div style = {styles.row}>
+                    <div style={styles.dates}>
+                        <input
+                            type="datetime-local"
+                            placeholder="Date"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            name="date"
+                            />
+                        </div>
+                        <div style = {styles.image}>
+                            <input 
+                                type="file" 
+                                onChange={(e) => setImage(e.target.files[0])}
+                                name="image"
+                                />
+                        </div>
+                        <div style = {styles.title}>
+                            <input 
+                                type="text"
+                                placeholder="Titre"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                name="name"
+                                />
+                        </div>
+                        <div style = {styles.description}>
+                            <input 
+                                type="text"
+                                placeholder="Description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                name="description"
+                                />
+                        </div>
+                        <button type="submit">Envoyer</button>
+                    </div>
             </form>
             
             {responseMessage && <p>{responseMessage}</p>}
             <ul>{name} </ul>
+            <br />
             <ul>{description}</ul>
         </div>
     );
